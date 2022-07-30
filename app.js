fdc44def1b90504dbf4e69df9adc848c73c0d502
Maksim Kozhevnikov <mkozhevnikov23@gmail.com>
2022-07-30 19:17:48 +0400

    dev: add middleware for CORS

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-// const cors = require('./middlewares/cors');
+const cors = require('./middlewares/cors');
 // const NotFoundError = require('./errors/not-found-err');
 
 const { PORT = 3000 } = process.env;
@@ -18,7 +18,7 @@ mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(requestLogger);
-// app.use(cors);
+app.use(cors);
 
 app.use('signup', require('./routes/signup'));
 app.use('signin', require('./routes/signin'));
