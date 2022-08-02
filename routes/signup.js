4fe674b868d484bca2178297b6cc8660670d4489
Maksim Kozhevnikov <mkozhevnikov23@gmail.com>
2022-08-02 22:58:13 +0400

    hotfix: refactor routes signup

--- routes/signup.js
+++ routes/signup.js
@@ -6,7 +6,7 @@ const { createUser } = require('../controllers/users');
 router.post('/', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
+    password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
   }),
 }), createUser);
