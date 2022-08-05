184db90aacd80ad0105d13e125aa3f57e2bbbbec
Maksim Kozhevnikov <mkozhevnikov23@gmail.com>
2022-08-05 16:51:28 +0400

    hotfix: merge signup and signin to auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 
-const { createUser } = require('../controllers/users');
+const { login, createUser } = require('../controllers/users');
+
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
 
 router.post('/signup', celebrate({
   body: Joi.object().keys({
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,7 @@ const router = require('express').Router();
 const auth = require('../middlewares/auth');
 const NotFoundError = require('../errors/not-found-err');
 
-router.use(require('./signup'));
-router.use(require('./signin'));
+router.use(require('./auth'));
 
 router.use(auth);
 router.use(require('./users'));
